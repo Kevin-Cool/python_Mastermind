2164d9f25ab80c123cc341ce2a9166324fb21d13
Kevin-Cool <kevincoolXD@gmail.com>
2020-06-22 16:26:40 +0200

    adding an UI

--- Board.py
+++ Board.py
@@ -5,15 +5,16 @@ class Board:
         self.difficulty = difficulty
         self.board_state = []
         self.board_evaluations = []
+        self.board_won = 0
+
     def generate(self):
         if self.difficulty==0:
             self.__solution = [random.randint(1,6),random.randint(1,6),random.randint(1,6),random.randint(1,6)]
         else:
             self.__solution = [random.randint(0,8),random.randint(0,8),random.randint(0,8),random.randint(0,8)]
 
-    def schow_bord(self):
-        print("Difficulty  : " + str(self.difficulty))
-        print("Solution    : " + str(self.__solution[0]) + "," + str(self.__solution[1]) + ","+ str(self.__solution[2]) + "," + str(self.__solution[3]))
+    def show_solution(self):
+        return self.__solution
 
     def make_guess(self,guess):
         self.board_state.append(guess)
@@ -46,12 +47,22 @@ class Board:
         #incase no guesses where correct
         if not temp_board_evaluation:
             temp_board_evaluation = [0,0,0,0]
-            
+
         #filling up tempBoardEvaluations with 0 until its at 4 numbers
         for count_fill in range(len(temp_board_evaluation),4):
             temp_board_evaluation.append(0)
 
-        self.board_evaluations.append(temp_board_evaluation)
+        #check if game is wone
+        if guess == self.__solution:
+            self.board_won = 1
+
+        #check if game is already lost before adding a guess
+        if self.board_won == 0:
+            self.board_evaluations.append(temp_board_evaluation)
+
+        #check if board is lost after this guess
+        if len(self.board_evaluations) == 9 and self.board_won != 1:
+            self.board_won = -1
 
     def give_row(self,row):
         if row<len(self.board_state):
@@ -59,7 +70,7 @@ class Board:
         else:
             return [0,0,0,0]
 
-    def give_bord(self):
+    def give_board(self):
         return self.boardState
 
     def give_evaluation(self,row):
@@ -67,3 +78,9 @@ class Board:
             return self.board_evaluations[row]
         else:
             return [0,0,0,0]
+
+    def give_boardstare(self):
+        return self.board_won
+
+    def give_current_row(self):
+        return len(self.board_state)
--- run.py
+++ run.py
@@ -1,25 +1,79 @@
+import os
+
 from Board import Board
 
-mastermind = Board(0)
-
-mastermind.generate()
-
-mastermind.schow_bord()
-
-print("----- make guess 1")
-guess = []
-print("guess 0:")
-guess.append(int(input()))
-print("guess 1:")
-guess.append(int(input()))
-print("guess 2:")
-guess.append(int(input()))
-print("guess 3:")
-guess.append(int(input()))
-mastermind.make_guess(guess)
-mastermind.schow_bord()
-print("guess :" + str(mastermind.give_row(0)) + "evaluation :" + str(mastermind.give_evaluation(0)))
-print(mastermind.give_row(1) + mastermind.give_evaluation(1))
-
+def start_mastermind():
+    temp_difficulty_select = 0
+    ant = " ";
+    #setting up the game by selecting a difficulty
+    while temp_difficulty_select != 1:
+        if temp_difficulty_select == -1:
+            print("That was not a valid number")
+        print("Difficulty options:")
+        print("Easy  :0")
+        print("Medium:1")
+        print("Select a difficulty setting :")
+        ant = input()
+        try:
+            if (int(ant) == 0) or (int(ant) == 1) :
+                mastermind = Board(int(ant))
+                mastermind.generate()
+                temp_difficulty_select = 1
+            else:
+                temp_difficulty_select = -1
+        except Exception as e:
+            temp_difficulty_select = -1
+        os.system('cls')
+    #running the game
+    game_won = mastermind.give_boardstare()
+    while  game_won == 0:
+        print("Game Difficulty: " + str(mastermind.difficulty) + ", currently at row: "+ str(mastermind.give_current_row()+1))
+        print("|   Game guesses:   ||    Evaluations:   |")
+        print(" =01= =02= =03= =04=  ==== ==== ==== ==== ")
+        for row_count in range(8,-1,-1):
+            temp_row = mastermind.give_row(row_count)
+            temp_row_evaluation = mastermind.give_evaluation(row_count)
+            print( "| " + str(temp_row[0]).zfill(2) + " | "  + str(temp_row[1]).zfill(2) + " | "  + str(temp_row[2]).zfill(2) + " | "  + str(temp_row[3]).zfill(2) + " || "  + str(temp_row_evaluation[0]).zfill(2) + " | "  + str(temp_row_evaluation[1]).zfill(2) + " | "  + str(temp_row_evaluation[2]).zfill(2) + " | "  + str(temp_row_evaluation[3]).zfill(2) + " | ")
+        print(" ==== ==== ==== ====  ==== ==== ==== ====")
+        guess = []
+        print("guess 1:")
+        try:
+            guess.append(int(input()))
+        except Exception as e:
+            guess.append(0)
+        print("guess 2:")
+        try:
+            guess.append(int(input()))
+        except Exception as e:
+            guess.append(0)
+        print("guess 3:")
+        try:
+            guess.append(int(input()))
+        except Exception as e:
+            guess.append(0)
+        print("guess 4:")
+        try:
+            guess.append(int(input()))
+        except Exception as e:
+            guess.append(0)
+        mastermind.make_guess(guess)
+        game_won = mastermind.give_boardstare()
+        os.system('cls')
+    print("Game Difficulty: " + str(mastermind.difficulty) + ", currently at row: "+ str(mastermind.give_current_row()+1))
+    print("| Game guesses:     || Evaluations:      |")
+    print(" =01= =02= =03= =04=  ==== ==== ==== ==== ")
+    for row_count in range(8,-1,-1):
+        temp_row = mastermind.give_row(row_count)
+        temp_row_evaluation = mastermind.give_evaluation(row_count)
+        print( "| " + str(temp_row[0]).zfill(2) + " | "  + str(temp_row[1]).zfill(2) + " | "  + str(temp_row[2]).zfill(2) + " | "  + str(temp_row[3]).zfill(2) + " || "  + str(temp_row_evaluation[0]).zfill(2) + " | "  + str(temp_row_evaluation[1]).zfill(2) + " | "  + str(temp_row_evaluation[2]).zfill(2) + " | "  + str(temp_row_evaluation[3]).zfill(2) + " | ")
+    print(" ==== ==== ==== ====  ==== ==== ==== ====")
+    if mastermind.give_boardstare() == 1:
+        print("Well done you won!")
+        print("In " + str(mastermind.give_current_row()) + " guesses.")
+        print("the correct answer was: " + str(mastermind.show_solution()))
+    else:
+        print("You lost :(")
+        print("the correct answer was: " + str(mastermind.show_solution()))
+start_mastermind()
 
 print("I'm done!")
